64e83b668e08ff6e5546ec8ba18c1e708f78865a
dgedgedge <denis.germa@free.fr>
2024-02-04 18:19:28 +0100

    Correction for simplify survey.

--- Software/pythonDrivers/ina219.py
+++ Software/pythonDrivers/ina219.py
@@ -1,3 +1,5 @@
+#!/bin/env python3
+
 """Library for the INA219 current and power monitor from Texas Instruments.
 
 Supports the Raspberry Pi using the I2C bus.
@@ -475,3 +477,37 @@ class DeviceRangeError(Exception):
         super(DeviceRangeError, self).__init__(msg)
         self.gain_volts = gain_volts
         self.device_limit_reached = device_max
+
+
+
+
+
+if __name__ == "__main__":
+    # Dictionnaire associant les noms aux informations sur les capteurs INA219
+    ina219_set = {
+        "alim_1_i2c_41   ": {"address": 0x41, "shunt_ohms": 0.02, "max_expected_amps": 6},
+        "alim_2_i2c_44   ": {"address": 0x44, "shunt_ohms": 0.02, "max_expected_amps": 6},
+        "alim_3_i2c_45_5V": {"address": 0x46, "shunt_ohms": 0.02, "max_expected_amps": 6},
+        "alim_4_i2c_47   ": {"address": 0x49, "shunt_ohms": 0.02, "max_expected_amps": 6},
+        "alim_5_i2c_47   ": {"address": 0x4d, "shunt_ohms": 0.02, "max_expected_amps": 6}
+    }
+
+
+    # Créer une instance INA219Reader pour chaque capteur
+    # Mettre à jour le dictionnaire ina219_addresses avec l'objet INA219Reader
+    for name, info in ina219_set.items():
+        address = info["address"]
+        shunt_ohms = info["shunt_ohms"]
+        max_expected_amps = info["max_expected_amps"]
+        ina219_set[name]["ina219_object"] = INA219(address=address, shunt_ohms=shunt_ohms, max_expected_amps=max_expected_amps, busnum=1)
+        ina219_set[name]["ina219_object"].configure()
+
+    for name, info in ina219_set.items():
+            ina219 = info["ina219_object"]
+            shunt_voltage = ina219.shunt_voltage()
+            bus_voltage = ina219.voltage()
+            current = ina219.current()
+            power = ina219.power()
+
+            print(f"{name}: Shunt {shunt_voltage:+.3f}V, Bus {bus_voltage:+.3f}V Current: {current:+.3f}A, Power: {power:.3f}mW")
+
