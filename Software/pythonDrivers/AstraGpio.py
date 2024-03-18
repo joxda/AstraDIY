422f0b0180b0e3909ea703f0f80c06fe58548952
dgedgedge <denis.germa@free.fr>
2024-03-18 23:11:08 +0100

    Add options to AstraGpio.py

--- Software/pythonDrivers/AstraGpio.py
+++ Software/pythonDrivers/AstraGpio.py
@@ -41,6 +41,12 @@ class AstraGpio():
     def get_name(self):
         return self.gpioline.name()
 
+    def set_on(self):
+        self.gpioline.set_value(1)
+
+    def set_off(self):
+        self.gpioline.set_value(0)
+
     def switch_onoff(self):
         if self.gpioline.get_value() == 0:
             self.gpioline.set_value(1)
@@ -52,13 +58,36 @@ class AstraGpio():
 
 
 if __name__ == "__main__":
-    listastragpio = []
-    for name in  [ "AstraDc1", "AstraDc2", "AstraDc3" ]:
-        listastragpio.append(AstraGpio(name))
-    while True:
-        for line in listastragpio:
-            line.switch_onoff()
-            time.sleep(1)
-            line.print_status()
+    import argparse
+    import os
 
+    parser = argparse.ArgumentParser( prog=os.path.basename(__file__), description='Program that enable to set AstrAlim Gpio.', epilog='', usage='%(prog)s [options]')
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    parser.add_argument('--test', action='store_true', help='If specified and no othe params, then a test set will be run.')
+    parser.add_argument('--gpioOn', nargs='+', choices=AstraGpio.astraGpioSet.keys(), default=[], help='Enable to set On the AstrAlim Gpio.')
+    parser.add_argument('--gpioOff', nargs='+', choices=AstraGpio.astraGpioSet.keys(), default=[], help='Enable to set Off the AstrAlim Gpio.')
+    args = parser.parse_args()
+
+    if len(args.gpioOn) > 0:
+        for interface in args.gpioOn:
+            if args.verbose:
+                print("Set On =", interface)
+            AstraGpio(interface).set_on()
+    elif len(args.gpioOff) > 0:
+        for interface in args.gpioOff:
+            if args.verbose:
+                print("Set Off =", interface)
+            AstraGpio(interface).set_off()
+    else:
+        if args.test:
+            listastragpio = []
+            for name in  [ "AstraDc1", "AstraDc2", "AstraDc3" ]:
+                listastragpio.append(AstraGpio(name))
+            while True:
+                for line in listastragpio:
+                    line.switch_onoff()
+                    time.sleep(1)
+                    line.print_status()
+        else:
+            parser.print_help()
 
